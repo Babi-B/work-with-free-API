10117d116290a31cc97840dacd0d7f992db5bd0f
Babi-B <babibyoolah@gmail.com>
2025-10-17 04:21:02 +0100

    A simple get API

---
+++ get_cat_api.py
@@ -0,0 +1,18 @@
+import requests
+
+#const
+URL = "https://catfact.ninja/fact"
+
+def get_cat_fact():
+    try:
+        response = requests.get(URL)
+        if response.status_code == "200":
+            return response
+    except requests.RequestException as e:
+        print(f"There was an error: {e}")
+    return response
+
+data = get_cat_fact().json()
+
+print(f"Fact: {data.get('fact')}")
+
